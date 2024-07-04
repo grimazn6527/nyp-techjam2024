5e93b9a6ddd9f9b8b1132ed5a5d3c205d2b9018a
Mohamed Faizal <42328708+TeaMakesMePee@users.noreply.github.com>
2024-07-04 15:09:16 +0800

    abstracted database

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
 import whisper_transcriber
 import sentiment
-import sqlite3
 import random
-import datetime
+import database
 
-# Create a connection to the database
-conn = sqlite3.connect('DATABASE.db')
-conn.execute('PRAGMA foreign_keys = ON')
-c = conn.cursor()
+database.Connect()
+database.CreateTables()
 
-# Create clients table if it doesnt exist
-c.execute("CREATE TABLE IF NOT EXISTS Clients (ClientID INTEGER PRIMARY KEY AUTOINCREMENT, ClientName TEXT, PhoneNumber INTEGER, OverallSentiment REAL)")
-# Create call record table if it doesnt exist
-c.execute("CREATE TABLE IF NOT EXISTS CallRecord (Sentiment REAL, CallDate DATE, ClientID INTEGER, FOREIGN KEY (ClientID) REFERENCES Clients(ClientID))")
-
-whisper_transcriber.transcribeAudio("numb.mp3")
+#whisper_transcriber.transcribeAudio("numb.mp3")
 sentiment_scores = sentiment.get_sentiment("audiotranscribed.txt")
 # Generate an 8-digit number starting with 8 or 9
 clientNumber = str(random.randint(80000000, 99999999))
 
-# Check if the phone number already exists in the Clients table
-c.execute("SELECT COUNT(*) FROM Clients WHERE PhoneNumber = ?", (clientNumber,))
-clientID = c.fetchone()[0]
-# Insert data into the database only if the phone number does not exist
-if clientID == 0:
-    c.execute("INSERT INTO Clients (ClientName, PhoneNumber, OverallSentiment) VALUES ('<NAME>', ?, ?)", (clientNumber, sentiment_scores['compound']))
-    # Get the last inserted clientID
-    c.execute("SELECT last_insert_rowid()")
-    clientID = c.fetchone()[0]
+clientID = database.AddClient(clientNumber, "<NAME>", sentiment_scores['compound'])
+database.AddCallRecord(sentiment_scores['compound'], clientID)
 
-# Get today's date
-today = datetime.date.today()
-# Insert data into the CallRecord table
-c.execute("INSERT INTO CallRecord (Sentiment, CallDate, ClientID) VALUES (?, ?, ?)", (sentiment_scores['compound'], today, clientID))
-
-conn.commit()
-conn.close()
+database.Disconnect()
--- whisper_transcriber.py
+++ whisper_transcriber.py
@@ -1,43 +1,43 @@
-import whisper
+# import whisper
 
-CHUNK_LIM = 480000
+# CHUNK_LIM = 480000
 
-def transcribeAudio(filename):
-    model = whisper.load_model("medium")
-    audios = []
-    audio = whisper.load_audio(filename)
+# def transcribeAudio(filename):
+#     model = whisper.load_model("medium")
+#     audios = []
+#     audio = whisper.load_audio(filename)
 
-    # if smaller than 30 sec, move on
-    if len(audio) <= CHUNK_LIM:
-        audio = whisper.pad_or_trim(audio)
-        audios.append(audio)
+#     # if smaller than 30 sec, move on
+#     if len(audio) <= CHUNK_LIM:
+#         audio = whisper.pad_or_trim(audio)
+#         audios.append(audio)
 
-    # if larger than 30 sec, chunk it and pad last piece
-    else:
+#     # if larger than 30 sec, chunk it and pad last piece
+#     else:
 
-        for i in range(0, len(audio), CHUNK_LIM):
-            chunk = audio[i : i + CHUNK_LIM]
-            chunk_index = len(chunk)
-            if chunk_index < CHUNK_LIM:
-                chunk = whisper.pad_or_trim(chunk)
-            audios.append(chunk)
+#         for i in range(0, len(audio), CHUNK_LIM):
+#             chunk = audio[i : i + CHUNK_LIM]
+#             chunk_index = len(chunk)
+#             if chunk_index < CHUNK_LIM:
+#                 chunk = whisper.pad_or_trim(chunk)
+#             audios.append(chunk)
 
-    results = ""
+#     results = ""
 
-    for chunk in audios:
-        #print(chunk.shape)
-        # chunk = whisper.pad_or_trim(chunk)
-        # make log-Mel spectrogram and move to the same device as the model
-        mel = whisper.log_mel_spectrogram(chunk).to(model.device)
+#     for chunk in audios:
+#         #print(chunk.shape)
+#         # chunk = whisper.pad_or_trim(chunk)
+#         # make log-Mel spectrogram and move to the same device as the model
+#         mel = whisper.log_mel_spectrogram(chunk).to(model.device)
 
-        # decode the audio
-        options = whisper.DecodingOptions(fp16=False)
-        result = whisper.decode(model, mel, options)
-        # print(result)
-        results += result.text + ". " 
+#         # decode the audio
+#         options = whisper.DecodingOptions(fp16=False)
+#         result = whisper.decode(model, mel, options)
+#         # print(result)
+#         results += result.text + ". " 
 
-    print(results)
+#     print(results)
 
-    f = open("audiotranscribed.txt", "w")
-    f.write(results)
-    f.close()
+#     f = open("audiotranscribed.txt", "w")
+#     f.write(results)
+#     f.close()
